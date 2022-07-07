8d7f8a4a88b40870274ade97cb75428d61457c18
techstackmedia <osagienoah04@gmail.com>
2022-07-07 12:48:43 +0100

    commit new chnages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import Contacts from '../components/Contacts/Contacts';
 import Footer from '../components/Footer/Footer';
 import Hero from '../components/Hero/Hero';
 import Navbar from '../components/Navbar/Navbar';
 
 const Contact = () => {
+  useEffect(() => {
+    document.title = 'Contact | CBC';
+  });
   return (
     <>
       <Navbar />
--- src/pages/Leadership.jsx
+++ src/pages/Leadership.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import LeaderShip from '../components/LeaderShip/LeaderShip';
 import Footer from '../components/Footer/Footer';
 import Hero from '../components/Hero/Hero';
 
 const Leadership = () => {
+  useEffect(() => {
+    document.title = 'Leadership | CBC';
+  });
   return (
     <>
       <Navbar />
