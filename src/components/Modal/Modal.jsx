0e40dcd99c4b44154f5f5c7c3e8b4e623a3eabba
techstackmedia <osagienoah04@gmail.com>
2022-07-23 22:57:58 +0100

    commit changes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import modal from './Modal.module.css';
 const Modal = ({ videoToggleStyle }) => {
+  const [isControlled, setIsControlled] = useState(false);
+  const handleVideoControlMouseEnter = () => {
+    if (isControlled === true) {
+      setIsControlled(false);
+    }
+  };
+
+  const handleVideoControlMouseLeave = () => {
+    if (isControlled === false) {
+      setIsControlled(true);
+    }
+  };
 
   return (
-    <section className={modal.section} style={videoToggleStyle}>
+    <section
+      className={modal.section}
+      style={videoToggleStyle}
+      onMouseLeave={handleVideoControlMouseLeave}
+      onMouseEnter={handleVideoControlMouseEnter}
+    >
       <div className={modal.modal}>
         <video
           src="https://res.cloudinary.com/bizstak/video/upload/v1654334058/VideoAbout_o5royr.mp4"
-          controls
+          controls={!isControlled}
+          muted={isControlled}
         >
           Video not supported
         </video>
