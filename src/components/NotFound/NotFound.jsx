81434b9dee8ba0298ff8837c1f480a0a46019470
techstackmedia <osagienoah04@gmail.com>
2022-07-29 11:14:44 +0100

    commit changes

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -12,14 +12,26 @@ const NotFound = () => {
       <div className={notfound.notfound}>
         <div className={notfound.notify}>
           <h2>404</h2>
-          <h3 className={notfound.text}>
-            This page you are looking for does not exist
-          </h3>
+          <h3 className={notfound.text}>This page does not exist</h3>
           <div>
             <button onClick={navHome}>Back to Home Page</button>
           </div>
         </div>
       </div>
+      <h1
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          position: 'relative',
+          top: '104px',
+        }}
+      >
+        Refer back to the home page {' '}
+        <span role="img" aria-label="wrong page">
+          🤔
+        </span>
+      </h1>
     </section>
   );
 };
